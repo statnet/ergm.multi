bec2dc671215b39e91f525cf012310bb6351f86b
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2021-02-28 23:02:43 +1100

    Plugged the package into the scntrl() API.

--- R/zzz.R
+++ R/zzz.R
@@ -13,7 +13,7 @@
 #' @import stats
 ## #' @import rlang
 
-.onAttach <- function(lib, pkg){
+.onAttach <- function(libname, pkgname){
   #' @importFrom statnet.common statnetStartupMessage
   sm <- statnetStartupMessage("ergm.multi", c("statnet"), FALSE)
   if(!is.null(sm)){
@@ -21,13 +21,37 @@
   }
 }
 
-.onLoad <- function(lib, pkg){
+.onLoad <- function(libname, pkgname){
   # . is used as a placeholder by stantet.common::NVL3().
   utils::globalVariables(".")
   options(ergm.eval.loglik=TRUE)
 
+  eval(COLLATE_ALL_MY_CONTROLS_EXPR)
+
   .RegisterProposals()
 }
+
+# TODO: Figure out some automatic way to keep this in sync with statnet.common.
+#' @name snctrl
+#'
+#' @title Statnet Control
+#'
+#' @description A utility to facilitate argument completion of control lists, reexported from `statnet.common`.
+#'
+#' @section Currently recognised control parameters:
+#' This list is updated as packages are loaded and unloaded.
+#'
+#' \Sexpr[results=rd,stage=render]{statnet.common::snctrl_names()}
+#'
+#' @seealso [statnet.common::snctrl()]
+#' @docType import
+NULL
+#' @export
+snctrl <- statnet.common::snctrl
+## BEGIN boilerplate: should be kept in sync with statnet.common.
+
+
+eval(UPDATE_MY_SCTRL_EXPR)
 
 .RegisterProposals <- function(){
   ergm_proposal_table("c", "Bernoulli", "|bd&blockdiag", 0, "random", "blockdiag")
