984eeac364b3b120d8eb6b23a086900e9dc72384
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2022-06-25 17:38:14 +1000

    blockdiag() constraint definition had been provisionally moved 'ergm', so it has been commented out here.

--- R/InitErgmConstraint.R
+++ R/InitErgmConstraint.R
@@ -21,80 +21,82 @@
 #
 ##########################################################################################
 
-#' @templateVar name blockdiag
-#' @title Block-diagonal structure constraint
-#' @description Force a block-diagonal structure (and its bipartite analogue) on
-#'   the network. Only dyads \eqn{(i,j)} for which
-#'   `attr(i)==attr(j)` can have edges.
-#'
-#'   Note that the current implementation requires that blocks be
-#'   contiguous for unipartite graphs, and for bipartite
-#'   graphs, they must be contiguous within a partition and must have
-#'   the same ordering in both partitions. (They do not, however,
-#'   require that all blocks be represented in both partitions, but
-#'   those that overlap must have the same order.)
-#'
-#'   If multiple block-diagonal constraints are given, or if
-#'   `attr` is a vector with multiple attribute names, blocks
-#'   will be constructed on all attributes matching.
-#'
-#' @usage
-#' # blockdiag(attr)
-#' @template ergmTerm-attr
-#'
-#' @template ergmConstraint-general
-#'
-#' @concept dyad-independent
-#' @concept directed
-#' @concept undirected
+#### This constraint has been provisionally moved to 'ergm'. It may be
+#### moved back here once 'ergm.multi' is on CRAN.
+## #' @templateVar name blockdiag
+## #' @title Block-diagonal structure constraint
+## #' @description Force a block-diagonal structure (and its bipartite analogue) on
+## #'   the network. Only dyads \eqn{(i,j)} for which
+## #'   `attr(i)==attr(j)` can have edges.
+## #'
+## #'   Note that the current implementation requires that blocks be
+## #'   contiguous for unipartite graphs, and for bipartite
+## #'   graphs, they must be contiguous within a partition and must have
+## #'   the same ordering in both partitions. (They do not, however,
+## #'   require that all blocks be represented in both partitions, but
+## #'   those that overlap must have the same order.)
+## #'
+## #'   If multiple block-diagonal constraints are given, or if
+## #'   `attr` is a vector with multiple attribute names, blocks
+## #'   will be constructed on all attributes matching.
+## #'
+## #' @usage
+## #' # blockdiag(attr)
+## #' @template ergmTerm-attr
+## #'
+## #' @template ergmConstraint-general
+## #'
+## #' @concept dyad-independent
+## #' @concept directed
+## #' @concept undirected
+## #' @import rle
+## InitErgmConstraint.blockdiag<-function(lhs.nw, attr=NULL, ...){
+##   if(length(list(...)))
+##     stop(paste("Block diagonal constraint takes one argument at this time."), call.=FALSE)
+##   list(attr=attr,
+##        free_dyads = {
+##          n <- network.size(lhs.nw)
+##          storage.mode(n) <- "integer"
+##          a <- c(ergm_get_vattr(attr, lhs.nw)) # Strip attributes, which confuse rle().
+##          if(NVL(lhs.nw%n%"bipartite",0)){
+##            bip <- lhs.nw %n% "bipartite"
+##            ea <- a[seq_len(bip)]
+##            aa <- a[bip+seq_len(n-bip)]
+##            if(length(rle(ea)$lengths)!=length(unique(rle(ea)$values)) || length(rle(aa)$lengths)!=length(unique(rle(aa)$values))) stop("Current implementation of block-diagonal sampling requires that the blocks of the egos and the alters be contiguous. See ", sQuote("ergmConstraint?blockdiag"), " for more information.")
+
+##            tmp <- .double.rle(ea, aa)
+##            el <- tmp$lengths1
+##            al <- tmp$lengths2
+
+##            o <- rlebdm(c(rep(rle(FALSE), bip*n, scale="run"),
+##                          do.call(c,rep(
+##                                      mapply(function(blen,bend){rep(rle(c(FALSE,TRUE,FALSE)), c(bend-blen, blen, n-bend), scale="run")},
+##                                             el, cumsum(el), SIMPLIFY=FALSE),
+##                                      al)
+##                                  )), n)
+##            # Future-proofing: in case it's bipartite directed, add
+##            # both thte blocks and their transposes. (If undirected,
+##            # it'll get filtered out by the .attributes constraints.)
+##            ot <- rlebdm(c(do.call(c,rep(
+##                                       mapply(function(blen,bend){rep(rle(c(FALSE,TRUE,FALSE)), c(bip+bend-blen, blen, n-bip-bend), scale="run")},
+##                                              al, cumsum(al), SIMPLIFY=FALSE),
+##                                       el)
+##                                   ),
+##                           rep(rle(FALSE), (n-bip)*n, scale="run")), n)
+##            compress(o | ot)
+##          }else{
+##            a <- rle(a)
+##            rlebdm(compress(do.call(c,rep(
+##                                        mapply(function(blen,bend){rep(rle(c(FALSE,TRUE,FALSE)), c(bend-blen, blen, n-bend), scale="run")},
+##                                               a$lengths, cumsum(a$lengths), SIMPLIFY=FALSE),
+##                                        a$lengths)
+##                                    )), n)
+##          }
+##        },
+##        dependence = FALSE)
+## }
+
 #' @import rle
-InitErgmConstraint.blockdiag<-function(lhs.nw, attr=NULL, ...){
-  if(length(list(...)))
-    stop(paste("Block diagonal constraint takes one argument at this time."), call.=FALSE)
-  list(attr=attr,
-       free_dyads = {
-         n <- network.size(lhs.nw)
-         storage.mode(n) <- "integer"
-         a <- c(ergm_get_vattr(attr, lhs.nw)) # Strip attributes, which confuse rle().
-         if(NVL(lhs.nw%n%"bipartite",0)){
-           bip <- lhs.nw %n% "bipartite"
-           ea <- a[seq_len(bip)]
-           aa <- a[bip+seq_len(n-bip)]
-           if(length(rle(ea)$lengths)!=length(unique(rle(ea)$values)) || length(rle(aa)$lengths)!=length(unique(rle(aa)$values))) stop("Current implementation of block-diagonal sampling requires that the blocks of the egos and the alters be contiguous. See ", sQuote("ergmConstraint?blockdiag"), " for more information.")
-
-           tmp <- .double.rle(ea, aa)
-           el <- tmp$lengths1
-           al <- tmp$lengths2
-
-           o <- rlebdm(c(rep(rle(FALSE), bip*n, scale="run"),
-                         do.call(c,rep(
-                                     mapply(function(blen,bend){rep(rle(c(FALSE,TRUE,FALSE)), c(bend-blen, blen, n-bend), scale="run")},
-                                            el, cumsum(el), SIMPLIFY=FALSE),
-                                     al)
-                                 )), n)
-           # Future-proofing: in case it's bipartite directed, add
-           # both thte blocks and their transposes. (If undirected,
-           # it'll get filtered out by the .attributes constraints.)
-           ot <- rlebdm(c(do.call(c,rep(
-                                      mapply(function(blen,bend){rep(rle(c(FALSE,TRUE,FALSE)), c(bip+bend-blen, blen, n-bip-bend), scale="run")},
-                                             al, cumsum(al), SIMPLIFY=FALSE),
-                                      el)
-                                  ),
-                          rep(rle(FALSE), (n-bip)*n, scale="run")), n)
-           compress(o | ot)
-         }else{
-           a <- rle(a)
-           rlebdm(compress(do.call(c,rep(
-                                       mapply(function(blen,bend){rep(rle(c(FALSE,TRUE,FALSE)), c(bend-blen, blen, n-bend), scale="run")},
-                                              a$lengths, cumsum(a$lengths), SIMPLIFY=FALSE),
-                                       a$lengths)
-                                   )), n)
-         }
-       },
-       dependence = FALSE)
-}
-
-
 InitErgmConstraint.upper_tri<-function(lhs.nw, attrname=NULL, ...){
   if(length(list(...)))
     stop(paste("Upper-triangular constraint takes one argument at this time."), call.=FALSE)
