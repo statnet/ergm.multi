a4ca6d80b07d22c68076ea5a8e9eccfa1861d2cd
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2022-06-25 18:08:46 +1000

    Documented upper_tri and blacklist_block constraints, and ported them to the new argument API.
    references statnet/ergm.multi#2

--- R/InitErgmConstraint.R
+++ R/InitErgmConstraint.R
@@ -96,16 +96,33 @@
 ##        dependence = FALSE)
 ## }
 
+#' @templateVar name upper_tri
+#' @title Only dyads in the upper-triangle of the sociomatrix may be
+#'   toggled
+#' @description For a directed network, only dyads \eqn{(i,j)} for
+#'   which \eqn{i < j} may be toggled. Optional argument `attr`
+#'   controls which subgraphs are thus restricted.
+#'
+#' @usage
+#' # upper_tri(attr = NULL)
+#' @template ergmTerm-attr
+#'
+#' @template ergmConstraint-general
+#'
+#' @concept dyad-independent
+#' @concept directed
 #' @import rle
-InitErgmConstraint.upper_tri<-function(lhs.nw, attrname=NULL, ...){
-  if(length(list(...)))
-    stop(paste("Upper-triangular constraint takes one argument at this time."), call.=FALSE)
-  if(!is.directed(lhs.nw)) stop("Upper-triangular constraint can be used only with directed networks.")
-  n <- network.size(lhs.nw)
+InitErgmConstraint.upper_tri<-function(nw, arglist, ...){
+  a <- check.ErgmTerm(nw, arglist, directed = TRUE,
+                      varnames = c("attr"),
+                      vartypes = c(ERGM_VATTR_SPEC),
+                      defaultvalues = list(NULL),
+                      required = c(FALSE))
+  n <- network.size(nw)
   storage.mode(n) <- "integer"
-  list(attrname=attrname,
+  list(attr=a$attr,
        free_dyads = {
-         restrict <- if(is.null(attrname)) rep(TRUE, n) else as.logical(lhs.nw %v% attrname)
+         restrict <- if(is.null(a$attr)) rep(TRUE, n) else ergm_get_vattr(a$attr, nw, accept = "logical")
          # The pattern is TRUE,...,TRUE,FALSE,...,FALSE for those
          # columns i where restrict[i]==TRUE, and it's just
          # TRUE,...,TRUE,TRUE,...,TRUE where restrict[i]==FALSE.
@@ -116,14 +133,45 @@ InitErgmConstraint.upper_tri<-function(lhs.nw, attrname=NULL, ...){
        )
 }
 
-InitErgmConstraint.blacklist_block<-function(lhs.nw, block_vattr=".ubid", blacklist_nattr=".block_blacklist"){
-  n <- as.integer(network.size(lhs.nw))
-  v <- get.vertex.attribute(lhs.nw,block_vattr,unlist=FALSE,na.omit=FALSE,null.na=FALSE)
-  bl <- get_all_bl(lhs.nw, blacklist_nattr)
+#' @templateVar name blacklist_block
+#' @title Blacklist blocks of dyads from toggling in a way that
+#'   propagates through combined networks
+#' @description This constraint is primarily for internal use,
+#'   alongside the block blacklist API implemented in
+#'   `block_blacklist.R`, which assigns and interprets vertex and
+#'   network attributes allowing certain blocks of dyads to be
+#'   blacklisted in a way that will survive even network manipulations
+#'   such as subgraph extraction and combining into block-diagonal
+#'   using [combine_networks()].
+#'
+#' @usage
+#' # blacklist_block(block_vattr = ".ubid",
+#' #                 blacklist_nattr = ".block_blacklist")
+#' @param block_vattr a character string giving the name of vertex
+#'   attribute containing globally unique vertex block membership IDs.
+#' @param blacklist_nattr a character string giving the name of a
+#'   network attribute containing the list of pairs of vertex IDs that
+#'   are to be blacklisted.
+#'
+#' @template ergmConstraint-general
+#'
+#' @concept dyad-independent
+#' @concept directed
+#' @concept undirected
+#' @keywords internal
+InitErgmConstraint.blacklist_block<-function(nw, arglist, ...){
+  a <- check.ErgmTerm(nw, arglist,
+                      varnames = c("block_vattr", "blacklist_nattr"),
+                      vartypes = c("character", "character"),
+                      defaultvalues = list(".ubid", ".block_blacklist"),
+                      required = c(FALSE, FALSE))
+  n <- as.integer(network.size(nw))
+  v <- get.vertex.attribute(nw, a$block_vattr, unlist=FALSE, na.omit=FALSE, null.na=FALSE)
+  bl <- get_all_bl(nw, a$blacklist_nattr)
 
   list(
-    block_vattr=block_vattr,
-    blacklist_nattr=blacklist_nattr,
+    block_vattr=a$block_vattr,
+    blacklist_nattr=a$blacklist_nattr,
     free_dyads=if(length(bl)) (lapply(bl, function(b){
       btail <- in_block(v, b[[1]])
       bhead <- in_block(v, b[[2]])
