5c89c8605c679b8e915f7cf063864171723cc31f
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2022-11-09 22:30:55 +1100

    Updated Roxygen templates and termalike documentation to link up correctly.

--- man-roxygen/ergmReference-rdname.R
+++ man-roxygen/ergmReference-rdname.R
@@ -1,4 +1,4 @@
-#  File man-roxygen/ergmConstraint-general.R in package ergm.multi, part of the
+#  File man-roxygen/ergmReference-rdname.R in package ergm.multi, part of the
 #  Statnet suite of packages for network analysis, https://statnet.org .
 #
 #  This software is distributed under the GPL-3 license.  It is free,
@@ -8,8 +8,4 @@
 #  Copyright 2003-2022 Statnet Commons
 ################################################################################
 #' <% name <- if(startsWith(name, "'")) substr(name, 2, 1000) else name %>
-#' @name <%= name %>-ergmConstraint
-#' @rdname <%= ergm:::.term.rdname("ergmConstraint", name) %>
-#' @seealso [`ergmConstraint`] for index of constraints and hints currently visible to the package.
-#'
-#' \Sexpr[results=rd,stage=render]{ergm:::.formatTermKeywords("ergmConstraint", "<%= name %>", "subsection")}
+#' @rdname <%= ergm:::.term.rdname("ergmReference", name) %>
