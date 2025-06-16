d74abaafe6a67b8511eabb08d084d00e3154f887
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2025-06-16 15:42:41 +1000

    Abstracted binary and valued ergm.multi_*changestat_multinet.h .

--- src/ergm.multi_changestat_multinet.h.template.do_not_include_directly.h
+++ src/ergm.multi_changestat_multinet.h.template.do_not_include_directly.h
@@ -7,10 +7,6 @@
  *
  *  Copyright 2003-2025 Statnet Commons
  */
-#ifndef _ERGM_MULTI_WTCHANGESTAT_MULTINET_H_
-#define _ERGM_MULTI_WTCHANGESTAT_MULTINET_H_
-
-#include "ergm_wtedgetree.h"
 #include "ergm.multi_changestat_multinet_common.do_not_include_directly.h"
 
 /* network-aware macros eponymous to ergm_changestat.h */
@@ -40,30 +36,7 @@
 
 typedef struct {
   unsigned int ns;
-  WtNetwork *inwp, **onwp;
+  ETYPE(Network) *inwp, **onwp;
   Vertex *sid;
   Vertex *smap;
-} StoreWtSubnets;
-
-#define WtMN_IO_TAIL(sn, t) ((Vertex) ((sn)->smap[t]))
-#define WtMN_IO_HEAD(sn, h) ((Vertex) ((sn)->smap[h]))
-#define WtMN_SID_TAIL(sn, t) ((Vertex) ((sn)->sid[t]))
-#define WtMN_SID_HEAD(sn, h) ((Vertex) ((sn)->sid[h]))
-
-#define WtMN_IGETWT(sn, l,a,b) (WtGetEdge(WtMN_OI_TAIL(sn, l, a), WtMN_OI_HEAD(sn, l, b), sn->inwp))
-#define WtMN_ISETWT(sn, l,a,b,w) (WtSetEdge(WtMN_OI_TAIL(sn, l, a), WtMN_OI_HEAD(sn, l, b),w,(sn)->inwp))
-
-/* If STRICT_Wt_HEADERS is not set, give the terms more generic names. */
-#ifndef STRICT_Wt_HEADERS
-
-#define MN_IO_TAIL WtMN_IO_TAIL
-#define MN_IO_HEAD WtMN_IO_HEAD
-#define MN_SID_TAIL WtMN_SID_TAIL
-#define MN_SID_HEAD WtMN_SID_HEAD
-
-#define MN_IGETWT WtMN_IGETWT
-#define MN_ISETWT WtMN_ISETWT
-
-#endif // STRICT_Wt_HEADERS
-
-#endif // _ERGM_MULTI_WTCHANGESTAT_MULTINET_H_
+} ETYPE(Store, Subnets);
